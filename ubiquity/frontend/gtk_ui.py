60fc8d5ba454a92fd241961c220cf53c545f59b2
Frédéric Gaudet <fred@linuxmint.com>
2013-10-21 10:54:16 +0200

    Fixed ubiquity icon

--- ubiquity/frontend/gtk_ui.py
+++ ubiquity/frontend/gtk_ui.py
@@ -896,7 +896,8 @@ class Wizard(BaseFrontend):
             box = self.builder.get_object(eventbox)
             style = box.get_style_context()
             style.add_class('ubiquity-menubar')
-
+        
+        self.live_installer.set_icon_name("ubiquity")
         # TODO lazy load
         from gi.repository import Vte
         self.vte = Vte.Terminal()

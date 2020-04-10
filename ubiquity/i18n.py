8f39ebed8a9402e7b396e7ab436e74072f30a47a
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2020-04-10 14:08:32 +0100

    l18n: Add a new l10n domain
    This allows us to introduce new msgids

--- ubiquity/i18n.py
+++ ubiquity/i18n.py
@@ -25,6 +25,8 @@ import re
 import subprocess
 import sys
 
+import gettext
+
 from ubiquity import im_switch, misc
 
 
@@ -218,6 +220,9 @@ def map_widget_name(prefix, name):
 
 def get_string(name, lang, prefix=None):
     """Get the translation of a single string."""
+    if name.startswith("mint:"):
+        name = name.replace("mint:", "")
+        return gettext.dgettext("mintubiquity", name)
     question = map_widget_name(prefix, name)
     translations = get_translations()
     if question not in translations:
